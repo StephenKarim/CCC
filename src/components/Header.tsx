d0efb340e28b838c29caab6f4dd05ecc2d06bf12
StephenKarim <103379758+StephenKarim@users.noreply.github.com>
2024-07-25 12:36:37 -0400

    2

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,13 @@
-
 import NavBar from "@/components/NavBar";
 import { createClient } from "@/prismicio";
 
 export default async function Header() {
-    const client = createClient();
-    const settings = await client.getSingle("settings");
+  const client = createClient();
+  const settings = await client.getSingle("settings");
+
   return (
     <header>
-      <NavBar settings={settings}/>
+      <NavBar settings={settings} />
     </header>
   );
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,10 +2,10 @@
 
 import { useState } from "react";
 import Link from "next/link";
-import { asLink, Content } from "@prismicio/client";
+import { Content, asLink } from "@prismicio/client";
 import { PrismicNextLink } from "@prismicio/next";
-import WordMark from "@/components/WordMark";
 import ButtonLink from "@/components/ButtonLink";
+import WordMark from "@/components/WordMark";
 import { MdMenu, MdClose } from "react-icons/md";
 import clsx from "clsx";
 import { usePathname } from "next/navigation";
@@ -17,9 +17,10 @@ type NavBarProps = {
 export default function NavBar({ settings }: NavBarProps) {
   const [open, setOpen] = useState(false);
   const pathname = usePathname();
+
   return (
-    <nav className="px-4 py-4 md:px-6 md:py-6" aria-label="Main">
-      <div className="max-auto flex max-w-6xl flex-col justify-between py-2 font-medium text-white md:flex-row md:items-center">
+    <nav className="md-:py-6 px-4 py-4 md:px-6" aria-label="Main">
+      <div className="mx-auto flex max-w-6xl flex-col justify-between py-2 font-medium text-white md:flex-row md:items-center">
         <div className="flex items-center justify-between">
           <Link href="/" className="z-50" onClick={() => setOpen(false)}>
             <WordMark />
@@ -32,15 +33,13 @@ export default function NavBar({ settings }: NavBarProps) {
             onClick={() => setOpen(true)}
           >
             <MdMenu />
-            <span className="sr-only">Open Menu</span>
+            <span className="sr-only">Open menu</span>
           </button>
         </div>
-
         {/* Mobile Nav */}
-
         <div
           className={clsx(
-            "fixed bottom-0 left-0 right-0 top-0 z-40 flex flex-col items-end gap-4 bg-[#070815] pr-4 pt-14 transition-transform duration-300 ease-in-out motion-reduce:transition-none md:hidden",
+            "ga-4 fixed bottom-0 left-0 right-0 top-0 z-40 flex flex-col items-end bg-[#070815] pr-4 pt-14 transition-transform duration-300 ease-in-out motion-reduce:transition-none md:hidden",
             open ? "translate-x-0" : "translate-x-[100%]",
           )}
         >
@@ -51,8 +50,9 @@ export default function NavBar({ settings }: NavBarProps) {
             onClick={() => setOpen(false)}
           >
             <MdClose />
-            <span className="sr-only">Close Menu</span>
+            <span className="sr-only">Close menu</span>
           </button>
+
           <div className="grid justify-items-end gap-8">
             {settings.data.navigation.map((item) => {
               if (item.cta_button) {
@@ -91,7 +91,7 @@ export default function NavBar({ settings }: NavBarProps) {
         </div>
 
         {/* Desktop Nav */}
-        <ul className="hidden gap-6 md:flex">
+        <ul className=" hidden gap-6 md:flex">
           {settings.data.navigation.map((item) => {
             if (item.cta_button) {
               return (
@@ -113,8 +113,8 @@ export default function NavBar({ settings }: NavBarProps) {
             return (
               <li key={item.label}>
                 <PrismicNextLink
-                  className="inline-flex min-h-11 items-center"
                   field={item.link}
+                  className="inline-flex min-h-11 items-center"
                   aria-current={
                     pathname.includes(asLink(item.link) as string)
                       ? "page"
