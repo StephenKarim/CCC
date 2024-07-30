7dd30f5cb887d85f56226de054eeb1f5d0db55a8
StephenKarim <103379758+StephenKarim@users.noreply.github.com>
2024-07-30 13:19:01 -0400

    2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="font-lora">
-      <body className="bg-[#070815] text-white">
+      <body className="bg-[#070815] text-white bg-[url('/images/background.png')] bg-fixed bg-cover">
         <Header />
         <main>{children}</main>
         <Footer />
--- src/slices/Bento/index.tsx
+++ src/slices/Bento/index.tsx
@@ -17,7 +17,7 @@ const Bento = ({ slice }: BentoProps): JSX.Element => {
     <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
-      className="bg-white bg-image-[/images/background.png]"
+      className=""
     >
       <PrismicRichText
         field={slice.primary.heading}
@@ -42,8 +42,8 @@ const Bento = ({ slice }: BentoProps): JSX.Element => {
         {slice.primary.bento_box.map((item) => (
           <div
             className={clsx(
-              "glass-container row-span-3 grid grid-rows-subgrid gap-4 rounded-lg bg-gradient-to-b from-gray-900 to-gray-950 p-4",
-              item.wide ? "md:col-span-2" : "md:col-span-1"
+              " row-span-3 grid grid-rows-subgrid gap-4 rounded-lg bg-gradient-to-b from-gray-900 to-gray-950 p-4",
+              item.wide ? "md:col-span-2" : "md:col-span-1",
             )}
             key={asText(item.title)}
           >
