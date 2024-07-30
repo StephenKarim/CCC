a45782e9527f46900410d3ecab3bd54bc80ad761
StephenKarim <103379758+StephenKarim@users.noreply.github.com>
2024-07-30 01:41:42 -0400

    Update AnimatedContentest.tsx

--- src/components/AnimatedContentest.tsx
+++ src/components/AnimatedContentest.tsx
@@ -116,13 +116,12 @@ export default function AnimatedContent() {
         </video>
       </div>
       <div
-        className={`${dancingScript.className} text-right text-4xl mt-[45vh] absolute inset-0 z-10 flex h-fit w-full flex-col items-start justify-start text-balance rounded-lg bg-transparent p-4 md:text-6xl lg:text-8xl font-bold shadow-lg `}
-      >
-        <div className="bg-black bg-opacity-25 w-[60vw]"> 
+        className={`${dancingScript.className} text-right text-4xl mt-[45vh] absolute inset-0 z-10 flex h-fit bg-black bg-opacity-50 w-[60vw] flex-col items-end text-balance rounded-r-lg p-4 md:text-6xl lg:text-8xl font-bold shadow-lg `}
+      >       
 
         <h1 className="">You`re Welcome</h1>
-        <h1 className="mr-[9rem] md:mr-[15rem] lg:mr-[24rem]">Here</h1>
-        </div>
+        <h1 className=" sm:mr-[9rem] md:mr-[15rem] lg:mr-[24rem]">Here</h1>
+    
       </div>
       <button className="hero__button mt-[3.5rem] opacity-0">text</button>
       {/* <div className="h-4 w-full bg-white mt-14">
