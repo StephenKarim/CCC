61fcc33f96153105acb5245f1926774a4627ddba
StephenKarim <103379758+StephenKarim@users.noreply.github.com>
2024-08-01 14:24:06 -0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="font-lora">
-      <body className="grid h-[100svh] bg-[url('/images/I3.jpg')] bg-cover bg-fixed bg-center bg-no-repeat text-white">
+      <body className="grid h-[100vh] bg-[url('/images/I3.jpg')] bg-cover bg-fixed bg-center text-white">
         {/* bg-[url('/images/background.png')] bg-fixed bg-cover */}
         <Header />
         <main>{children}</main>
