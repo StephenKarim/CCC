ed737950f8a9034273b934c1fbb4f0aa27bb0ef7
StephenKarim <103379758+StephenKarim@users.noreply.github.com>
2024-08-01 14:36:33 -0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,6 @@ import { repositoryName } from "@/prismicio";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
-import Script from "next/script";
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -13,26 +11,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="font-lora">
-      <body className="bg-[url('/images/I3.jpg')] bg-cover bg-fixed bg-center text-white">
-        <div className="grid h-[100svh]">
-          {/* bg-[url('/images/background.png')] bg-fixed bg-cover */}
-          <Header />
-          <main>{children}</main>
-          <Footer />
-          <PrismicPreview repositoryName={repositoryName} />
-        </div>
-        <Script id="set-vh" strategy="beforeInteractive">
-          {`
-            function setVh() {
-              const vh = window.innerHeight * 0.01;
-              document.documentElement.style.setProperty('--vh', \`\${vh}px\`);
-            }
-
-            window.addEventListener('resize', setVh);
-            window.addEventListener('orientationchange', setVh);
-            setVh();
-          `}
-        </Script>
+      <body className="h-[100svh] bg-[url('/images/I3.jpg')] bg-cover bg-fixed bg-center text-white">
+        {/* bg-[url('/images/background.png')] bg-fixed bg-cover */}
+        <Header />
+        <main>{children}</main>
+        <Footer />
+        <PrismicPreview repositoryName={repositoryName} />
       </body>
     </html>
   );
