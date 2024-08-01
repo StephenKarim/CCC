91ba0d4f2f6e74ac71eded42f819be7683ccfd5f
StephenKarim <103379758+StephenKarim@users.noreply.github.com>
2024-08-01 02:21:08 -0400

    Update index.tsx

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -21,7 +21,7 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
 const Hero = ({ slice }: HeroProps): JSX.Element => {
   return (
     <Bounded
-      className=" text-center pb-2 bg-[#070815] bg-opacity-95"
+      className=" text-center bg-[#070815] bg-opacity-95"
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
