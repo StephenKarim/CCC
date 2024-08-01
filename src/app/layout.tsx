825bf8af75c7ec8143d7e51c725da23a0ab94086
StephenKarim <103379758+StephenKarim@users.noreply.github.com>
2024-08-01 14:28:35 -0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,12 +11,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="font-lora">
-      <body className="grid h-[100vh] bg-[url('/images/I3.jpg')] bg-cover bg-fixed bg-center text-white">
-        {/* bg-[url('/images/background.png')] bg-fixed bg-cover */}
-        <Header />
-        <main>{children}</main>
-        <Footer />
-        <PrismicPreview repositoryName={repositoryName} />
+      <body className="bg-[url('/images/I3.jpg')] bg-cover bg-fixed bg-center text-white">
+        <div className="grid h-[100svh]">
+          {/* bg-[url('/images/background.png')] bg-fixed bg-cover */}
+          <Header />
+          <main>{children}</main>
+          <Footer />
+          <PrismicPreview repositoryName={repositoryName} />
+        </div>
       </body>
     </html>
   );
