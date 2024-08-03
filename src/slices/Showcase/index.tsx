d7c20d2e41cd1982d507e0f098126cbf7d561e35
StephenKarim <103379758+StephenKarim@users.noreply.github.com>
2024-08-02 22:29:22 -0400

    Update index.tsx

--- src/slices/Showcase/index.tsx
+++ src/slices/Showcase/index.tsx
@@ -38,20 +38,20 @@ const Showcase = ({ slice }: ShowcaseProps): JSX.Element => {
 
       {/* <AnimatedContent>
         </AnimatedContent> */}
-        <PrismicRichText
-          field={slice.primary.heading}
-          components={{
-            heading2: ({ children }) => (
-              <h2
-                className={`${russoOne.className} text-balance text-center text-5xl font-medium md:text-7xl`}
-              >
-                {children}
-              </h2>
-            ),
-          }}
-        />
+      <PrismicRichText
+        field={slice.primary.heading}
+        components={{
+          heading2: ({ children }) => (
+            <h2
+              className={`${russoOne.className} text-balance text-center text-5xl font-medium md:text-7xl`}
+            >
+              {children}
+            </h2>
+          ),
+        }}
+      />
       <div className="mt-16 grid items-center gap-8 rounded-xl border border-blue-50/20 bg-gradient-to-b from-slate-50/15 to-slate-50/5 px-8 py-8 backdrop-blur-sm lg:grid-cols-3 lg:gap-0 lg:py-12">
-        <div>          
+        <div>
           <div className="mt-6 text-2xl font-normal">
             <PrismicRichText field={slice.primary.subheading} />
           </div>
@@ -70,9 +70,8 @@ const Showcase = ({ slice }: ShowcaseProps): JSX.Element => {
           alt=""
           className={clsx(
             "opacity-90 shadow-2xl lg:col-span-2 lg:pt-0",
-            slice.variation === "reverse"
-              ? "lg:order-1 lg:translate-x-[15%]"
-              : "lg:-order-1 lg:translate-x-[-15%]",
+
+            "lg:order-1 lg:translate-x-[15%]",
           )}
           sizes="(max-width: 768px) 100vw, 50vw"
         />
