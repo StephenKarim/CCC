0fd938db5220726ddaf0034e649b24904018ffa5
StephenKarim <103379758+StephenKarim@users.noreply.github.com>
2024-08-07 06:32:54 -0400

    2

--- src/slices/Showcase/index.tsx
+++ src/slices/Showcase/index.tsx
@@ -32,7 +32,7 @@ const Showcase = ({ slice }: ShowcaseProps): JSX.Element => {
     <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
-      className="relative bg-[url('/images/I3.jpg')] bg-cover bg-fixed bg-scroll bg-center px-4 py-14 text-secondary text-shadow-lg first:pt-10 md:px-6 md:py-20 lg:py-24"
+      className="bg-fixed-ios relative bg-[url('/images/I3.jpg')] bg-cover bg-fixed bg-center px-4 py-14 text-secondary text-shadow-lg first:pt-10 md:px-6 md:py-20 lg:py-24"
     >
       {/* <AnimatedContent>
         </AnimatedContent> */}
