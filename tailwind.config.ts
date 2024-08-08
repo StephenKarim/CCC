7d373212fe68289b72114ae400bb0533808d8338
StephenKarim <103379758+StephenKarim@users.noreply.github.com>
2024-08-08 00:19:26 -0400

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,132 +1,135 @@
-import type { Config } from 'tailwindcss';
-import plugin from 'tailwindcss/plugin';
+import type { Config } from "tailwindcss";
+import plugin from "tailwindcss/plugin";
 
 /** @type {Config} */
 const config: Config = {
-  darkMode: ['class'],
+  darkMode: ["class"],
   content: [
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/slices/**/*.{js,ts,jsx,tsx,mdx}',
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/slices/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
-  prefix: '',
+  prefix: "",
   theme: {
     container: {
       center: true,
-      padding: '2rem',
+      padding: "2rem",
       screens: {
-        '2xl': '1400px',
+        "2xl": "1400px",
       },
     },
     extend: {
       textShadow: {
-        sm: '0 1px 2px rgba(0, 0, 0, 0.15)',
-        DEFAULT: '0 2px 4px rgba(0, 0, 0, 0.25)',
-        lg: '0 8px 16px rgba(0, 0, 0, 0.35)',
+        sm: "0 1px 2px rgba(0, 0, 0, 0.15)",
+        DEFAULT: "0 2px 4px rgba(0, 0, 0, 0.25)",
+        lg: "0 8px 16px rgba(0, 0, 0, 0.35)",
       },
       fontFamily: {
-        lora: ['Lora', 'serif'],
+        lora: ["Lora", "serif"],
         // Add more fonts here if needed
       },
       colors: {
-        border: 'hsl(0, 0%, 89.8%)',
-        input: 'hsl(0, 0%, 89.8%)',
-        ring: 'hsl(0, 0%, 3.9%)',
-        background: 'hsl(var(--background) / <alpha-value>)',
-        foreground: 'hsl(var(--foreground) / <alpha-value>)',
+        border: "hsl(0, 0%, 89.8%)",
+        input: "hsl(0, 0%, 89.8%)",
+        ring: "hsl(0, 0%, 3.9%)",
+        background: "hsl(var(--background) / <alpha-value>)",
+        foreground: "hsl(var(--foreground) / <alpha-value>)",
         card: {
-          DEFAULT: 'hsl(var(--card) / <alpha-value>)',
-          foreground: 'hsl(var(--card-foreground) / <alpha-value>)',
+          DEFAULT: "hsl(var(--card) / <alpha-value>)",
+          foreground: "hsl(var(--card-foreground) / <alpha-value>)",
         },
         popover: {
-          DEFAULT: 'hsl(var(--popover) / <alpha-value>)',
-          foreground: 'hsl(var(--popover-foreground) / <alpha-value>)',
+          DEFAULT: "hsl(var(--popover) / <alpha-value>)",
+          foreground: "hsl(var(--popover-foreground) / <alpha-value>)",
         },
         primary: {
-          DEFAULT: 'hsl(var(--primary) / <alpha-value>)',
-          foreground: 'hsl(var(--primary-foreground) / <alpha-value>)',
+          DEFAULT: "hsl(var(--primary) / <alpha-value>)",
+          foreground: "hsl(var(--primary-foreground) / <alpha-value>)",
         },
         secondary: {
-          DEFAULT: 'hsl(var(--secondary) / <alpha-value>)',
-          foreground: 'hsl(var(--secondary-foreground) / <alpha-value>)',
+          DEFAULT: "hsl(var(--secondary) / <alpha-value>)",
+          foreground: "hsl(var(--secondary-foreground) / <alpha-value>)",
         },
         destructive: {
-          DEFAULT: 'hsl(var(--destructive) / <alpha-value>)',
-          foreground: 'hsl(var(--destructive-foreground) / <alpha-value>)',
+          DEFAULT: "hsl(var(--destructive) / <alpha-value>)",
+          foreground: "hsl(var(--destructive-foreground) / <alpha-value>)",
         },
         muted: {
-          DEFAULT: 'hsl(var(--muted) / <alpha-value>)',
-          foreground: 'hsl(var(--muted-foreground) / <alpha-value>)',
+          DEFAULT: "hsl(var(--muted) / <alpha-value>)",
+          foreground: "hsl(var(--muted-foreground) / <alpha-value>)",
         },
         accent: {
-          DEFAULT: 'hsl(var(--accent) / <alpha-value>)',
-          foreground: 'hsl(var(--accent-foreground) / <alpha-value>)',
+          DEFAULT: "hsl(var(--accent) / <alpha-value>)",
+          foreground: "hsl(var(--accent-foreground) / <alpha-value>)",
         },
       },
       borderRadius: {
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
       keyframes: {
-        'accordion-down': {
-          from: { height: '0' },
-          to: { height: 'var(--radix-accordion-content-height)' },
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
         },
-        'accordion-up': {
-          from: { height: 'var(--radix-accordion-content-height)' },
-          to: { height: '0' },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
         },
       },
       animation: {
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
   plugins: [
     plugin(function ({ addUtilities }) {
       const newUtilities = {
-        '.bg-opacity-10': {
-          '--tw-bg-opacity': '0.1',
+        ".bg-opacity-10": {
+          "--tw-bg-opacity": "0.1",
         },
-        '.bg-opacity-20': {
-          '--tw-bg-opacity': '0.2',
+        ".bg-opacity-20": {
+          "--tw-bg-opacity": "0.2",
         },
-        '.bg-opacity-30': {
-          '--tw-bg-opacity': '0.3',
+        ".bg-opacity-30": {
+          "--tw-bg-opacity": "0.3",
         },
-        '.bg-opacity-40': {
-          '--tw-bg-opacity': '0.4',
+        ".bg-opacity-40": {
+          "--tw-bg-opacity": "0.4",
         },
-        '.bg-opacity-50': {
-          '--tw-bg-opacity': '0.5',
+        ".bg-opacity-50": {
+          "--tw-bg-opacity": "0.5",
         },
-        '.bg-opacity-60': {
-          '--tw-bg-opacity': '0.6',
+        ".bg-opacity-60": {
+          "--tw-bg-opacity": "0.6",
         },
-        '.bg-opacity-70': {
-          '--tw-bg-opacity': '0.7',
+        ".bg-opacity-70": {
+          "--tw-bg-opacity": "0.7",
         },
-        '.bg-opacity-80': {
-          '--tw-bg-opacity': '0.8',
+        ".bg-opacity-80": {
+          "--tw-bg-opacity": "0.8",
         },
-        '.bg-opacity-90': {
-          '--tw-bg-opacity': '0.9',
+        ".bg-opacity-90": {
+          "--tw-bg-opacity": "0.9",
         },
-        '.bg-opacity-100': {
-          '--tw-bg-opacity': '1',
+        ".bg-opacity-100": {
+          "--tw-bg-opacity": "1",
         },
       };
 
-      addUtilities(newUtilities, ['responsive', 'hover']);
+      addUtilities(newUtilities, {
+        respectImportant: true, // If you need to respect `!important`
+        respectPrefix: true, // If you need to respect any custom prefix
+      });
     }),
-    require('@tailwindcss/typography'),
-    require('tailwindcss-animate'),
+    require("@tailwindcss/typography"),
+    require("tailwindcss-animate"),
   ],
 };
 
