2ca8108a3668bd35b37be2f4d90ac7a8c09e0f17
StephenKarim <103379758+StephenKarim@users.noreply.github.com>
2024-08-08 20:47:08 -0400

    2

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -81,7 +81,7 @@ export default function NavBar({ settings }: NavBarProps) {
 
   return (
     <nav
-      className={`${russoOne.className} header__heading absolute z-10 h-[80px] w-full bg-background bg-opacity-95 p-2 text-foreground opacity-0 md:h-[90px]`}
+      className={`${russoOne.className} header__heading absolute z-10 h-[80px] w-full bg-[#26619C] bg-opacity-95 p-2 text-foreground opacity-0 md:h-[90px]`}
       aria-label="Main"
       ref={container}
     >
